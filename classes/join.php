7d6092cf63060a64718219c1464e06ad852da27e
Konrad Ebel <konrad.ebel@oncampus.de>
2025-09-02 15:18:38 +0200

    Basics for join trait for the sql query builder

---
+++ classes/join.php
@@ -0,0 +1,40 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+namespace local_sqlquerybuilder;
+
+
+use local_sqlquerybuilder\joins\join_expression;
+
+/**
+ * Trait that builds a sql statement, that can be exported via
+ * build_join()
+ *
+ * @copyright   Konrad Ebel
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+trait join {
+    /** @var join_expression[] All join expressions for the request */
+    protected array $joins = [];
+
+    protected function get_allowed_table_aliases(): array {
+
+    }
+
+    protected function export_join(): string {
+
+    }
+}
